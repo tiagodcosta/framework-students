9f484162095a07d3b8a16273aff2b890bf5b15d5
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-02-16 14:04:10 -0500

    Add students CSS library

--- gulpfile.js
+++ gulpfile.js
@@ -14,16 +14,16 @@ gulp.task('browser-sync', function(){
 	});
 
 gulp.task('sass', function(){
-	return gulp.src('./source/*.scss')
+	return gulp.src('./source-students/*.scss')
 	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 	.pipe(autoprefixer())
-	.pipe(gulp.dest('./css'));
+	.pipe(gulp.dest('./students/css'));
 });		
 
 gulp.task('watch', ['sass', 'browser-sync'], function () {
-	gulp.watch('./source/*.scss', ['sass']);
-    gulp.watch("./css/*.css").on('change', reload);
-    gulp.watch("./*.html").on('change', reload);
+	gulp.watch('./source-students/*.scss', ['sass']);
+    gulp.watch("./students/css/*.css").on('change', reload);
+    gulp.watch("./students/*.html").on('change', reload);
 });
 
 
