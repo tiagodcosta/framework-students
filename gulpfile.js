2cfc476a3db528d7248ff7892f557393121b19fb
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-08-18 15:36:49 -0400

    Remove dist from git

--- gulpfile.js
+++ gulpfile.js
@@ -15,20 +15,23 @@ gulp.task('browser-sync', function(){
 
 gulp.task('sass', function(){
 	return gulp.src('./src/scss/main.scss')
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(autoprefixer())
-	.pipe(gulp.dest('./dist/css'));
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('copy', function () {
+	gulp.src('./src/index.html')
+		.pipe(gulp.dest('./dist'));
 	gulp.src('./src/img/*.{png,gif,jpg}')
 		.pipe(gulp.dest('./dist/img'));
 });
 
 gulp.task('watch', ['sass', 'copy', 'browser-sync'], function () {
 	gulp.watch('./src/scss/*/*.scss', ['sass']);
+	gulp.watch('./src/index.html', ['copy']);
 	gulp.watch("./dist/css/*.css").on('change', reload);
-	gulp.watch("./dist/*.html").on('change', reload);
+	gulp.watch("./src/*.html").on('change', reload);
 });
 
 
