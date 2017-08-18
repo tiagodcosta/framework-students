94730743a0038fd9e102a6e3c7f86971b95e1f43
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-08-18 15:26:26 -0400

    Refine gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,34 @@
 var gulp = require('gulp'),
-	browserSync = require('browser-sync'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	reload = browserSync.reload;
+browserSync = require('browser-sync'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+reload = browserSync.reload;
 
 
 gulp.task('browser-sync', function(){
-		browserSync({
-			server: {
-				baseDir: "./dist"
-			}
-		});
+	browserSync({
+		server: {
+			baseDir: "./dist"
+		}
 	});
+});
 
 gulp.task('sass', function(){
-	return gulp.src('./src/scss/index.scss')
+	return gulp.src('./src/scss/main.scss')
 	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 	.pipe(autoprefixer())
 	.pipe(gulp.dest('./dist/css'));
-});		
+});
 
-gulp.task('watch', ['sass', 'browser-sync'], function () {
-	gulp.watch('./src/scss/*.scss', ['sass']);
-    gulp.watch("./dist/css/*.css").on('change', reload);
-    gulp.watch("./dist/*.html").on('change', reload);
+gulp.task('copy', function () {
+	gulp.src('./src/img/*.{png,gif,jpg}')
+		.pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('watch', ['sass', 'copy', 'browser-sync'], function () {
+	gulp.watch('./src/scss/*/*.scss', ['sass']);
+	gulp.watch("./dist/css/*.css").on('change', reload);
+	gulp.watch("./dist/*.html").on('change', reload);
 });
 
 
